2ae16f1abbdb430ba01409a1c93c94e2bb859f1d
octonoodle <162030697+octonoodle@users.noreply.github.com>
2024-07-08 20:05:15 -0400

    highly simplified protocol, bmp data only

--- penguin-driver/penguin-driver.ino
+++ penguin-driver/penguin-driver.ino
@@ -70,23 +70,13 @@ bool TRANSMIT_MODE;
 void setup() {
   serialInit();
   bmpInit();
-  flashInit();
-  if (TRANSMIT_MODE) {
-    rf95Init();
-    transmitData();
-  } else {
-    gpsInit();
-  }
+  rf95Init();
+  transmitData();
 }
 
 void loop() {
-  if (TRANSMIT_MODE) {
-    // this will terminate with a reboot once data is successfully transmitted
-    transmitData();
-  } else {
-    // this too
-    parseAndStore();
-  }
+  // this will terminate with a reboot once data is successfully transmitted
+  transmitData();
 }
 
 // reboot the chip (same as pressing RST button)
@@ -148,63 +138,7 @@ void bmpInit() {
     while (1);
   }
 }
-void flashInit() {
-  fastPrint("Configuring flash memory... ");
-  if (!flash.begin()) {
-    fastPrintln("failed");
-    while (1);
-  }
-  fastPrintln("success");
 
-  // First call begin to mount the filesystem.  Check that it returns true
-  // to make sure the filesystem was mounted.
-  if (!fatfs.begin(&flash)) {
-    fastPrintln("Error, failed to mount newly formatted filesystem");
-    fastPrintln("Was the flash chip formatted with the fatfs_format example?");
-    while (1)
-      ;
-  }
-  fastPrintln("mounted filesystem");
-  File32 eraseFlag = fatfs.open("noErase.txt", FILE_WRITE);
-  bool truncate = false;
-  if (!eraseFlag) {
-    truncate = true;
-    eraseFlag = fatfs.open("noErase.txt", FILE_WRITE | O_CREAT);
-  }
-  eraseFlag.close();
-
-  fastPrint("formatting csv for data recording... ");
-  csvFile = 
-  truncate ? 
-  fatfs.open("bmp390.csv", FILE_WRITE | O_CREAT | O_TRUNC) :
-  fatfs.open("bmp390.csv", FILE_WRITE | O_CREAT);
-  if (csvFile) {
-    if (truncate) {
-      csvFile.println("sensor data: ");
-      csvFile.println("\"date\",\"time\",\"altitude (m)\",\"pressure (hPa)\",\"temperature (°C)\",\"longitude (°)\",\"latitude (°)\",\"fix valid\"");
-      fastPrintln("data file first line written");
-    } else {
-      fastPrintln("did not print first line");
-    }
-  } else {
-    fastPrintln("failed to start write to data file!");
-    while (1);
-  }
-  csvFile.close();
-
-  fastPrint("config file... ");
-  configFile  = fatfs.open(CONFIG_FILE, FILE_READ);
-  if (configFile) {
-    TRANSMIT_MODE = true;
-    fastPrintln("found");
-    fastPrintln("Starting in Radio Mode");
-  } else {
-    TRANSMIT_MODE = false;
-    fastPrintln("missing");
-    fastPrintln("Starting in GPS Mode");
-  }
-  configFile.close();
-}
 void rf95Init() {
   fastPrint("Radio Client Initializing...");
   pinMode(RFM95_RST, OUTPUT);
@@ -251,44 +185,9 @@ void rf95Init() {
 
   fastPrintln("Radio configured");
 }
-void gpsInit() {
-  fastPrint("Initializing GPS... ");
-  GPSSerial.begin(9600);
-  pinPeripheral(A1, PIO_SERCOM_ALT);
-  pinPeripheral(A2, PIO_SERCOM_ALT);
-
-  // GPSSerial.println("$PMTK220,2000*"); // config command for gps
-  // while(!GPSSerial.available());
-  // char ackBuf[16];int i;
-  // while(GPSSerial.available()) {
-  //   char c = GPSSerial.read();
-  //   ackBuf[i] = c;
-  //   i++;
-  //   if (c == '\n' || i == 16) break;
-  // }
-  // if (strcmp(ackBuf, "$PMTK010,003*2C\n")) { // gps replying successfully set to 0.2hz update
-  //   fastPrintln("failed");
-  //   while(1);
-  // }
-  fastPrintln("success");
-}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~ RADIO TRANSMISSION ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void transmitData() {
-  #if TOGGLE_ALL_MESSAGES
-  if (Serial && Serial.available()) {
-    fastPrintln("You are typing something");
-    String input = Serial.readString();
-    if (input.equals("kill")) {
-      fastPrintln("terminating datalogging...");\
-      while (1)
-        ;
-    } else {
-      fastPrintln("input ignored");
-    }
-  }
-  verboseMessage("kill check");
-  #endif
 
   if (!bmp.performReading()) {
     fastPrintln("reading error");
@@ -306,95 +205,11 @@ void transmitData() {
   }
   verboseMessage("bmp valid");
 
-  // recover and parse gps
-  float longitude; // decimal degrees
-  float latitude; // decimal degrees
-  bool validFix;
-  int time; // in format 123456 HHMMSS
-  int date; // in format 123456 MMDDYY
-
-  configFile = fatfs.open(CONFIG_FILE, FILE_READ);
-  verboseMessage("opened config");
-
-  if (!configFile) {
-    fastPrintln("error, failed to read gps data file");
-  } else {
-    // parse raw nmea sentence
-    char rmcSentence[128];
-    if (USE_TEST_MESSAGE) {
-      strcpy(rmcSentence, "$GPRMC,123519,A,4807.038,N,01131.000,E,0.022,269.131,230394,,,A,C*");
-      verboseMessage("gps input replaced by test message");
-    } else {
-      configFile.readBytes(rmcSentence, sizeof(rmcSentence));
-      verboseMessage("rmc get");
-    }
-    rmcDecode(rmcSentence, sizeof(rmcSentence), &longitude, &latitude, &validFix, &time, &date); // extract data and put into variables
-    verboseMessage("rmc decoded:");
-    // print everything in one line
-    // timing
-    char foo[10];
-    sprintf(foo, "%f", longitude);
-    verboseMessage(foo);
-    sprintf(foo, "%f", latitude);
-
-    #if TOGGLE_ALL_MESSAGES
-    verboseMessage(foo);
-    Serial.println(validFix);
-    Serial.println(time);
-    Serial.println(date);
-    short month = floor(date % 10000 /100); 
-    short day = floor(date/10000);
-    short year = date % 100;
-    short hour = floor(time/10000); 
-    short min = floor(time % 10000 /100); 
-    short sec = time % 100;
-    char timedate[17];
-    addZerosPrint(month); Serial.print("/"); addZerosPrint(day); Serial.print("/"); addZerosPrint(year); Serial.print(" ");
-    addZerosPrint(hour); Serial.print(":"); addZerosPrint(min); Serial.print(":"); addZerosPrint(sec);
-
-    // posn
-    Serial.print(" long: "); Serial.print(longitude, 4);
-    Serial.print(" lat: "); Serial.print(latitude, 4);
-    Serial.print(" fix: "); Serial.println(validFix ? "valid" : "invalid");
-    #endif
-  }
-  verboseMessage("config closed "); 
-  verboseMessage(configFile.close() ? "1" : "0");
-
   #if TOGGLE_ALL_MESSAGES
   Serial.print("Altitude: "); Serial.println(altitude);
   Serial.print("Pressure: "); Serial.println(pressure);
   Serial.print("Temperature: "); Serial.println(temperature);
   #endif
-
-  csvFile = fatfs.open("bmp390.csv", FILE_WRITE | O_CREAT);
-  // record data to local file
-  if (csvFile) {
-    csvFile.print("\"");
-    addZerosPrintToFile((int)floor(date % 10000 /100)); csvFile.print('/'); 
-    addZerosPrintToFile((int)floor(date/10000)); csvFile.print('/');
-    addZerosPrintToFile(date % 100);
-    csvFile.print("\",\"");
-    addZerosPrintToFile((int)floor(time/10000)); csvFile.print(':');
-    addZerosPrintToFile((int)floor(time % 10000 /100)); csvFile.print(':'); 
-    addZerosPrintToFile((int)(time % 100));
-    csvFile.print("\","); 
-    csvFile.print(altitude);
-    csvFile.print(",");
-    csvFile.print(pressure);
-    csvFile.print(",");
-    csvFile.print(temperature);
-    csvFile.print(",");
-    csvFile.print(longitude, 4);
-    csvFile.print(",");
-    csvFile.print(latitude, 4);
-    csvFile.print(",");
-    csvFile.println(validFix);
-  } else {
-    Serial.println("failed to write data to file");
-  }
-  csvFile.close();
-
 
   uint8_t statusMessage[] = "data incoming";
   rf95.send(statusMessage, sizeof(statusMessage));
@@ -408,7 +223,7 @@ void transmitData() {
     if (rf95.recv(buf, &len)) {
       if (!strcmp((char *)buf, "ready for data")) {
           fastPrintln("handshake complete");
-          float dataPacket[] = {altitude, pressure, temperature, longitude, latitude, (float)validFix, (float)time, (float)date};
+          float dataPacket[] = {altitude, pressure, temperature};
           sendAndWait(dataPacket);
         }
       else {
@@ -426,26 +241,16 @@ void transmitData() {
 }
 
 //xxxx.xx,yyyy.yy,zz.zz
-void sendAndWait(float dataSet[8]) {
+void sendAndWait(float dataSet[3]) {
   // xxyy.zz
   float alt = dataSet[0];
   float pressure = dataSet[1];
   float temp = dataSet[2];
-  float longitude = dataSet[3];
-  float latitude = dataSet[4];
-  bool validFix = (bool)(int)dataSet[5];
-  int time = dataSet[6];
-  int date = dataSet[7];
-  bool negativeAlt = alt < 0;
-  bool negativeLong = longitude < 0;
-  bool negativeLat = latitude < 0;
 
   // each float needs to be broken up into integer sections: 1234.56 -> [12], [34], [56]
 
   // since we already know if values are negative, take abs to get pure digits and avoid negative overflow on unsigned ints
   alt = fabs(alt);
-  longitude = fabs(longitude);
-  latitude = fabs(latitude);
 
   // xxyy.zz
   uint8_t alt1 = (uint8_t)(floor(alt / 100)); //xx
@@ -461,49 +266,8 @@ void sendAndWait(float dataSet[8]) {
   uint8_t temp1 = (uint8_t)(floor(temp)); //xx
   uint8_t temp2 = (uint8_t)((temp - temp1)*100); //yy
 
-  uint8_t long1;
-  uint8_t long2;
-  uint8_t long3;
-
-  uint8_t lat1;
-  uint8_t lat2;
-  uint8_t lat3;
-  uint8_t lat4;
-
-  if (validFix) {
-    // aa.bbcc
-    long1 = (uint8_t)(floor(longitude)); //aa 
-    long2 = (uint8_t)(floor(100*(longitude - long1))); //bb
-    long3 = (uint8_t)((int)(10000*longitude) % 100); //cc
-
-    // abb.ccdd
-    lat1 = (uint8_t)floor(latitude/100); //a
-    lat2 = (uint8_t)(floor(latitude - lat1*100)); //bb
-    lat3 = (uint8_t)(floor(100*(latitude - lat1*100 - lat2))); //cc
-    lat4 = (uint8_t)((int)(10000*latitude) % 100); //dd
-  }
-
-  // xxyyzz
-  uint8_t hour = floor(time/10000);
-  uint8_t min = floor(time % 10000 /100);
-  uint8_t sec = time % 100;
-  // xxyyzz
-  uint8_t day = floor(date/10000);
-  uint8_t month = floor(date % 10000 /100);
-  uint8_t year = date % 100;
-
-  // alt: -220.29
-  // press: 1027.43
-  // temp: 28.74
-  // $GPRMC,123519,A,4807.038,N,01131.000,E,0.022,269.131,230394,,,A,C*
-  //alt long lat
-  //+/- +/- +/-    alt       press    temp      long        lat      fix    time      date     end
-  // 1 | 0 | 0 | 2 20 29 | 10 27 43 | 28 73 | 48 11 73 | 0 11 51 66 | 1 | 12 35 19 | 3 23 94 | 0
-
   uint8_t encodedPackets[] = 
-  {(uint8_t)negativeAlt, (uint8_t)negativeLong, (uint8_t)negativeLat, alt1, alt2, alt3, pressure1, pressure2, pressure3, temp1, temp2, 
-  long1, long2, long3, lat1, lat2, lat3, lat4, (uint8_t)validFix,
-  hour, min, sec, month, day, year, 0};
+  {alt1, alt2, alt3, pressure1, pressure2, pressure3, temp1, temp2};
   
   #if TOGGLE_ALL_MESSAGES
   Serial.print("encoded message to send (");
@@ -526,8 +290,8 @@ void sendAndWait(float dataSet[8]) {
     if (rf95.recv(buf, &len)) {
       if (!strcmp((char *)buf, "data recieved!!")) { // END CONDITION: TRANSMIT SUCCESS AND REBOOT
         fastPrintln("transmit success");
-        fatfs.remove(CONFIG_FILE);
-        reboot();
+        //fatfs.remove(CONFIG_FILE);
+        //reboot();
       } else {
         fastPrint("unexpected reply: ");
         fastPrintln((char *)buf);
@@ -539,200 +303,4 @@ void sendAndWait(float dataSet[8]) {
     fastPrintln("no response from server during data transfer!");
     return;
   }
-}
-
-// parse fields from rmc message and copy into given buffer variables
-void rmcDecode(char* source, size_t srcLen, float *longit, float *lat, bool *validFix, int *time, int *date) {
-  /*
-  example message:
-  $GPRMC,123519,A,4807.038,N,01131.000,E,0.022,269.131,230394,,,A,C*
-  name   time  fix   long      lat      speed  sat angle date        always ends with *
-  */
-
-  verboseMessage("before decode");
-  char fields[10][10]; // only concerned with first 10 fields
-  int field = 0;
-  int letter = 0;
-  char temp[10];
-  int i = 0;
-  for (i; i<srcLen;i++) {
-    #if VERBOSE_MODE
-    Serial.print('_');
-    Serial.print(source[i]);
-    #endif
-
-    if (source[i] == ',' || letter >= 10) {
-      if (field < 10) {
-        // store string of field efficiently
-        size_t len = strlen(temp);
-        #if VERBOSE_MODE
-        Serial.print("  ["); Serial.print(len); Serial.println("]");
-        #endif
-        // char anonField[len];
-        // strcpy(anonField, temp);
-        // verboseMessage(anonField);
-        strcpy(fields[field], temp);
-        field++;
-        letter = 0;
-        memset(temp, 0, 10 * sizeof(char)); // clear buffer
-        continue;
-      } else {
-        break;
-      }
-    }
-    temp[letter] = source[i];
-    letter++;
-  }
-  verboseMessage("field parse");
-  
-  // parse: 
-
-  // time+date
-  *time = atoi(fields[1]);
-  *date = atoi(fields[9]);
-  
-
-  // position data
-  float longitBuf = (float)atof(fields[3]);
-  *longit = (float)floor(longitBuf/100.0) + fmodf(longitBuf, 100.0)/60; // DDMM.MMM --> DD.DDDD
-  if (fields[4][0] == 'S') { // south
-    *longit = *longit * -1;
-  }
-
-  float latBuf = (float)atof(fields[5]);
-  *lat = (float)floor(latBuf/100.0) + fmodf(latBuf, 100.0)/60; // DDDMM.MMM --> DDD.DDDD
-  if (fields[6][0] == 'W') { // west
-    *lat = *lat * -1;
-  }
-  *validFix = (fields[2][0] == 'A');
-}
-
-// normalize nums like "9" and "4" to "09", "04", etc. (for time+date)
-void addZerosPrint(int num) {
-  #if TOGGLE_ALL_MESSAGES
-  if (abs(num) < 10) {
-    Serial.print("0");
-  } 
-  Serial.print(num);
-  #endif
-}
-
-void addZerosPrintToFile(int num) {
-  if (abs(num) < 10) {
-    csvFile.print("0");
-  } 
-  csvFile.print(num);
-}
-// ~~~~~~~~~~~~~~~~~~~~~~~~ GPS LOOP ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// to get RMC sentence:
-// 1. clear buffer
-// 2. wait for gps data
-// 3. slowly read until full sentence is formed
-// 4. parse
-// 5. finish or try again
-
-// checks if NMEA sentence is RMC (Recommended Minimum Coordinates) aka GPS data
-bool isRMC(char * sent) {
-  char msgType[6];
-  for (int i = 0; i < 6; i++) {
-    msgType[i] = sent[i];
-  }
-  return !strcmp(msgType, "$GNRMC");
-}
-
-void parseAndStore() {
-  
-  // allocate how many seconds to attempting to find RMC?
-  int timeoutSec = 3; // value below 5 not recommended for GPS updates at 1Hz
-  int timeoutChars = timeoutSec*960/5;
-
-  verboseMessage("start parse method");
-  delay(10);
-  // 1.
-  while(GPSSerial.available()) {
-    GPSSerial.read();
-  }
-  verboseMessage("cleared buffer");
-
-  // 2.
-  int countdown = 2000;
-  while(!GPSSerial.available()) {
-    delay(1);
-    countdown--;
-    if (countdown < 0) {
-      fastPrintln("no signal from gps, are you sure GPS TX is connected to pin A2?");
-      reboot();
-    }
-  }
-  // 3.
-  int i = 0;
-  int j = 0;
-  bool done = false;
-  // only copy found sentence to permanent buffer if it's an rmc (no timeout)
-  bool rmcValid = false;
-  char sentence[128];
-  verboseMessage("begin gps parse");
-  while(!done) {
-    delay(5);
-    if (GPSSerial.available()) {
-      char c = GPSSerial.read();
-      switch (c) {
-        case '$':
-          // restart message from beginning
-          j = 0;
-          for (j; j < 128; j++) {
-             sentence[j] = '\0';
-           }
-        case '*':
-          done = true;
-          rmcValid = true;
-          // 4.
-          if (!isRMC(sentence)) {
-            // continue to read from serial
-            delay(25);
-            done = false;
-            rmcValid = false;
-            j = 0;
-          }
-        default:
-          if (j < 128) {
-            sentence[j] = c;
-            j++;
-          } else {
-            fastPrintln("overflow");
-            j = 0;
-          }
-      }
-      i++;
-      if (i > timeoutChars) {
-        done = true;
-        #if TOGGLE_ALL_MESSAGES
-        Serial.println();
-        Serial.print("search timeout reached (");
-        Serial.print(timeoutChars);
-        Serial.println(") chars");
-        #endif
-      }
-    } else {
-      done = true;
-      #if TOGGLE_ALL_MESSAGES
-      Serial.println();
-      Serial.print("ended early at index: "); Serial.println(i);
-      #endif
-    }
-  }
-  
-  if (rmcValid) {
-    fastPrintln();
-    fastPrint("found: ");
-    fastPrintln(sentence);
-    // write new gps data to file for radio to transmit
-    configFile = fatfs.open(CONFIG_FILE, FILE_WRITE | O_CREAT);
-    configFile.write(sentence);
-    configFile.close();
-    reboot();
-  }
-  fastPrintln("DONE!");
-  delay(500);
-  
 }
--- penguin-ground-server/penguin-ground-server.ino
+++ penguin-ground-server/penguin-ground-server.ino
@@ -20,24 +20,37 @@ RH_RF95 rf95(8, 3); // Adafruit Feather M0 with RFM95
 //#define Serial SerialUSB
 
 int led = 13;
+#define SERIAL_MSG false // enable info messages
 
 //#define RFM95_RST 11  // "A"
 //#define RFM95_CS 10   // "B"
 //#define RFM95_INT 6   // "D"
 //RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
+void fastPrint(const char* msg) {
+  #if SERIAL_MSG
+  Serial.print(msg);
+  #endif
+}
 
+void fastPrintln(const char* msg) {
+  #if SERIAL_MSG
+  Serial.println(msg);
+  #endif
+}
 
 void setup() {
   pinMode(led, OUTPUT);
+  #if SERIAL_MSG
   Serial.begin(115200);
   while (!Serial);  // Wait for serial port to be available
   Serial.print("radio initializing... ");
+  #endif
 
   if (!rf95.init())
-    Serial.println("init failed");
+    fastPrintln("init failed");
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-  Serial.println("init success");
+  fastPrintln("init success");
   rf95.setFrequency(915.0);
 
   // You can change the modulation parameters with eg
@@ -46,7 +59,7 @@ void setup() {
   // The default transmitter power is 13dBm, using PA_BOOST.
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 2 to 20 dBm:
-  rf95.setTxPower(20, false);
+  //rf95.setTxPower(20, false);
   // If you are using Modtronix inAir4 or inAir9, or any other module which uses the
   // transmitter RFO pins and not the PA_BOOST pins
   // then you can configure the power transmitter power for 0 to 15 dBm and with useRFO true.
@@ -54,51 +67,24 @@ void setup() {
   //  driver.setTxPower(14, true);
 }
 
-void unpack(uint8_t buffer[RH_RF95_MAX_MESSAGE_LEN], float *alt, float *press, float *temp, float *longit, float *lat, bool *validFix, int *timings) {
-  // encoded input message format: 
-  //+/- +/- +/-    alt         press       temp        long           lat      fix    time            date     end
-  // 1 | 0 | 0 | 2 20 . 29 | 10 27 . 43 | 28 . 73 | 48 . 11 73 | 0 11 . 51 66 | 1 | 12 : 35 : 19 | 3 / 23 / 94 | 0
-  // 0   1   2   3  4    5    6  7    8   9    10   11   12 13  14 15   16 17   18  19   20   21  22   23   24 
-  // buffer index /\
-
-  *alt = ((bool)buffer[0] ? -1 : 1) * (100*buffer[3] + buffer[4] + (buffer[5]/100.0));
-  *press = 100*buffer[6] + buffer[7] + (buffer[8]/100.0);
-  *temp = buffer[9] + (buffer[10]/100.0);
-  *longit = ((bool)buffer[1] ? -1 : 1) * (buffer[11] + buffer[12]/100.0 + buffer[13]/10000.0);
-  *lat = ((bool)buffer[2] ? -1 : 1) * (100*buffer[14] + buffer[15] + buffer[16]/100.0 + buffer[17]/10000.0);
-  *validFix = buffer[18];
-  for (int i = 0; i < 6; i++) {
-    timings[i] = buffer[i+19];
+void unpack(float target[3], uint8_t buffer[RH_RF95_MAX_MESSAGE_LEN]) {
+  #if SERIAL_MSG
+  for (int i = 0; i < 8; i++) {
+    Serial.print(buffer[i]);
+    Serial.print(", ");
   }
-}
-
-// normalize nums like "9" and "4" to "09", "04", etc. (for time+date)
-void addZerosPrint(int num) {
-  if (abs(num) < 10) {
-    Serial.print("0");
-  } 
-  Serial.print(num);
-}
-
-void display(float alt, float press, float temp, float longit, float lat, bool validFix, int *timings) {
-  // time
-  addZerosPrint(timings[3]); Serial.print("/"); addZerosPrint(timings[4]); Serial.print("/"); addZerosPrint(timings[5]); Serial.print(" ");
-  addZerosPrint(timings[0]); Serial.print(":"); addZerosPrint(timings[1]); Serial.print(":"); addZerosPrint(timings[2]);
-
-  // posn
-  Serial.print(" long: "); 
-  Serial.print(longit, 4);
-  Serial.print(" lat: "); 
-  Serial.print(lat, 4);
-  Serial.print(" fix: "); Serial.println(validFix ? "valid" : "invalid");
-  
-  // bmp data
-  Serial.print("Altitude: "); Serial.println(alt);
-  Serial.print("Pressure: "); Serial.println(press);
-  Serial.print("Temperature: "); Serial.println(temp);
+  Serial.println();
+  #endif
+  float sum = 100*buffer[0] + buffer[1] + (buffer[2]/100.0);
+  target[0] = sum;
+  sum = 100*buffer[3] + buffer[4] + (buffer[5]/100.0);
+  target[1] = sum;
+  sum = buffer[6] + (buffer[7]/100.0);
+  target[2] = sum;
 }
 
 void loop() {
+  #if SERIAL_MSG
   if (Serial.available()) {
     Serial.println("You are typing something");
     String input = Serial.readString();
@@ -112,76 +98,52 @@ void loop() {
       Serial.println("input ignored");
     }
   }
+  #endif
 
   if (rf95.waitAvailableTimeout(10000)) {
-    Serial.println("Message request recieved, decoding...");
+    fastPrintln("Message request recieved, decoding...");
     // Should be a message for us now
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    //Serial.println(RH_RF95_MAX_MESSAGE_LEN);
     uint8_t len = sizeof(buf);
     if (rf95.recv(buf, &len)) {
       digitalWrite(led, HIGH);
+      #if SERIAL_MSG
       RH_RF95::printBuffer("request: ", buf, len);
       Serial.print("got request: ");
       Serial.println((char *)buf);
-      if (!strcmp((char*)buf, "data incoming")) {
-        // Send a reply
-        uint8_t okSignal[] = "ready for data";
-        rf95.send(okSignal, sizeof(okSignal));
-        rf95.waitPacketSent();
-        Serial.println("replied to handshake...");
-        digitalWrite(led, LOW);
+      #endif
 
-        // wait for real data
-        if (rf95.waitAvailableTimeout(5000)) {
-          for(int i; i < RH_RF95_MAX_MESSAGE_LEN; i++) {
-            buf[i] = 0; // clear buffer
-          }
-          len = sizeof(buf);
-          if(rf95.recv(buf, &len)) {
-            digitalWrite(led, HIGH);
-            Serial.print("successfully recieved message (");
-            Serial.print(len);
-            Serial.println(" bytes):");
+      // Send a reply
+      uint8_t okSignal[] = "ready for data";
+      rf95.send(okSignal, sizeof(okSignal));
+      rf95.waitPacketSent();
+      fastPrintln("replying to handshake...");
+      digitalWrite(led, LOW);
 
-            for (int i = 0; i < len; i++) {
-              Serial.print(buf[i]);
-            Serial.print(", ");
-            }
-            Serial.println();
-            Serial.println();
+      // wait for real data
+      while(!rf95.available());
 
-            // respond to client
-            uint8_t newMessage[] = "data recieved!!";
-            rf95.send(newMessage, sizeof(newMessage));
-            rf95.waitPacketSent();
-            Serial.println("acknowldeged data recieve success");
-            Serial.println();
-            
-            // parse recieved data
-            float alt;
-            float press;
-            float temp;
-            float longitude; // decimal degrees
-            float latitude; // decimal degrees
-            bool validFix;
-            int timings[6] = {0}; // in format 12 34 56   12 34 56
-            //                            HHMMSS     MMDDYY
-            unpack(buf, &alt, &press, &temp, &longitude, &latitude, &validFix, timings);
-            display(alt, press, temp, longitude, latitude, validFix, timings);
+      if(rf95.recv(buf, &len)) {
+        digitalWrite(led, HIGH);
+        fastPrintln("successfully recieved data: ");
+        float unpackedData[3] = {0, 0, 0};
+        unpack(unpackedData, buf);
 
-          } else {
-            Serial.println("data recv failed");
-          }
-          digitalWrite(led, LOW);
-        } else {
-          Serial.println("did not recieve data after handshake");
+        for (int i = 0; i < 3; i++) {
+          Serial.println(unpackedData[i]);
         }
+        Serial.println();
+
+        uint8_t newMessage[] = "data recieved!!";
+        rf95.send(newMessage, sizeof(newMessage));
+        rf95.waitPacketSent();
+        fastPrintln("acknowldeged data recieve success");
       } else {
-        Serial.println("malformed handshake");
+        fastPrintln("data recv failed");
       }
+      digitalWrite(led, LOW);
     } else {
-      Serial.println("request recv failed");
+      fastPrintln("request recv failed");
     }
   } else {
     Serial.println("no data recieved, listening again for 10s");
